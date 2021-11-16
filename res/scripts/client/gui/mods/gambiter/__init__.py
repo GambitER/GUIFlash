a436dff9349d38e7d1b7197275b2f82ff94aa696
CHAMPi <no@one.com>
2021-11-16 15:03:14 +0100

    v0.4.2 for WoT client v1.14.1.3 and higher - fix position animation issues once and for all (by PolyacovYury) - version number increased to 0.4.2 - gambiter.guiflash_0.4.2.wotmod included

--- res/scripts/client/gui/mods/gambiter/__init__.py
+++ res/scripts/client/gui/mods/gambiter/__init__.py
@@ -4,4 +4,4 @@ from flash import GUIFlash
 
 g_guiFlash = GUIFlash()
 
-print 'GUIFlash v0.4.1 by GambitER initialised. (maintained by CHAMPi)'
+print 'GUIFlash v0.4.2 by GambitER initialised. (maintained by CHAMPi)'
